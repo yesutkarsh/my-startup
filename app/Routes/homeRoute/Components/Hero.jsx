aa01d776b5842c8c87638f0cd42db2904996dea0
yesutkarsh <contactutkarshverma@gmail.com>
2024-07-03 12:41:18 +0530

    bug fixed

--- app/Routes/homeRoute/Components/Hero.jsx
+++ app/Routes/homeRoute/Components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <div className={style.wrapper}>
       <div className={style.hero}>
         <h1>Upload & <br /> <span>Print</span></h1>
-        <span>India&apos;s First Online Print On Demand Service</span> <br />
+        <span>India First Online Print On Demand Service</span> <br />
       </div>
     </div>
   );
