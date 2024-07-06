9232da814dbd1e5b1b9cd87c229ba1ebb009fe14
yesutkarsh <contactutkarshverma@gmail.com>
2024-07-06 15:15:15 +0530

    added about, contact, privacy policy page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react"
 
 export default function page() {
   return (
     <>
-    <div className='mt-[100px] m-5 p-10 bg-white rounded-[22px]'>
+    <div className="mt-[100px] m-5 p-10 bg-white rounded-[22px]">
     <h1>About</h1> <br />
     <p style={{color:"grey"}}>Welcome to our innovative platform, a one-stop solution for students looking to transform their study materials into professional, high-quality prints. Our service allows students to upload their notes, which we then print and convert into books delivered straight to their doorstep.</p>
     <br />
-    <p style={{color:"grey"}}>Our platform is designed to make studying more accessible and efficient, offering an array of services tailored to meet the needs of modern students. Whether you need your notes printed for easy reference, want to convert them into books for long-term use, or need visiting cards, we've got you covered. Join our community and experience the convenience of having your study materials professionally printed and delivered.</p>
+    <p style={{color:"grey"}}>Our platform is designed to make studying more accessible and efficient, offering an array of services tailored to meet the needs of modern students. Whether you need your notes printed for easy reference, want to convert them into books for long-term use, or need visiting cards, we&apos;ve got you covered. Join our community and experience the convenience of having your study materials professionally printed and delivered.</p>
     </div>
 
     </>
