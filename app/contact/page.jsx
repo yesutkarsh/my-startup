bdc772b2855d27929e28086bb91858040b79efb3
yesutkarsh <contactutkarshverma@gmail.com>
2024-07-06 15:03:31 +0530

    added about, contact, privacy policy page

---
+++ app/contact/page.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+    <div className='mt-[100px] m-5 p-10 bg-white rounded-[22px]'>
+    <h1>Contact and Return</h1> <br />
+    <p style={{color:"grey"}}>Due to the personalized nature of our products, we do not accept returns. Each note, book, and print order is customized specifically for you, which makes it impossible to restock or resell these items. However, we are committed to ensuring your complete satisfaction with our services.</p>
+    <br />
+    <p style={{color:"grey"}}>If you receive a product that is damaged or contains errors in printing, we are happy to offer a replacement. Please follow the steps below to request a replacement:</p> <br />
+    <li style={{color:"grey"}}>Contact Us: Reach out to our customer service team within 7 days of receiving your order.</li>
+    <br />
+    <li style={{color:"grey"}}>Provide Details: Include your order number, a description of the issue, and photographs of the damaged or incorrect items.</li>
+    <br />
+    <li style={{color:"grey"}}>Review Process: Our team will review your request and get back to you within 2 business days.</li>
+    <br />
+    <h1>Contact Information</h1>
+    <br />
+    <p style={{color:"grey"}}>If you have any questions or need assistance with a replacement, please contact us:</p>
+    <br />
+    <li style={{color:"grey"}}>Email : contactutkarshverma@gmail.com</li>
+    <br />
+    <li style={{color:"grey"}}>Email : contactutkarshverma@proton.me</li>
+    <br />
+    <li style={{color:"grey"}}>Phone : +91 7985748915</li>
+    <br />
+    <li style={{color:"grey"}}>Address : Awas Vikas Colony Gonda, Uttarpradesh</li>
+
+
+
+    </div>
+
+    </>
+  )
+}
---
+++ app/policy/page.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <div className="mt-[100px] m-5 p-10 bg-white rounded-[22px]">
+        <h1>PRIVACY NOTICE</h1> <br />
+        <p style={{ color: "grey" }}>
+          This privacy notice for our website, describes now and why we might
+          collect, store, use, and/or share ("process") your information when
+          you use our services ("Services"), such as when you:
+        </p>
+        <br />
+        <p style={{ color: "grey" }}>
+          Questions or concerns? Reading this privacy notice will help you
+          understand your privacy rights and choices. If you do not agree with
+          our policies and practices, please do not use our Services..
+        </p>
+        <br />
+        <p style={{ color: "grey" }}>
+          What personal information do we process? When you visit, use, or
+          navigate our Services, we may process personal information depending
+          on how you interact with us and the Services, the choices you make,
+          and the products and features you use.
+        </p>
+        <br />
+        <p style={{ color: "grey" }}>
+          Do we process any sensitive personal information? We do not process
+          sensitive personal information-
+        </p>
+        <br />
+        <p style={{ color: "grey" }}>
+          Do we process any sensitive personal information? We do not process
+          sensitive personal information- While using Our Service, We may ask
+          You to provide Us with certain personally identifiable information
+          that can be used to contact or identify You. Personally identifiable
+          information may include, but is not limited to: Email address First
+          name and last name Phone number Address, State, Province, ZIP/Postal
+          code, City Usage Data
+        </p>
+      </div>
+    </>
+  );
+}
